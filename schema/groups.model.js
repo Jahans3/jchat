25b36637363418d85bcfef66269e6198a5194fc0
Joshua Jahans <joshua.jahans@thehutgroup.com>
2016-06-06 21:39:07 +0100

    Group model

---
+++ schema/groups.model.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jahansj on 06/06/2016.
+ */
+"use strict";
+
+const mongoose = require('mongoose');
+
+let groupSchema = mongoose.Schema({
+
+    name: String,
+    founder: String,
+    channels: [{
+        name: String,
+        users: [ String ]
+    }],
+    users: [{
+        name: String,
+        permissions: Number,
+        channels: [ String ]
+    }]
+
+});
+
+module.exports mongoose.model('Group', groupSchema);
